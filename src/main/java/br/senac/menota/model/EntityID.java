3fc1f13297b83f2b4ed03c42595a22b75e23e363
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2025-03-17 20:21:48 -0300

    fix: EntityID getter, setter, to string, all args e no args constructor

--- src/main/java/br/senac/menota/model/EntityID.java
+++ src/main/java/br/senac/menota/model/EntityID.java
@@ -4,7 +4,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @MappedSuperclass
 public class EntityID {
 
