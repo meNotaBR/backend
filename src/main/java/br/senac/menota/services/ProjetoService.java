53a67bc5dde71f6a73153f3334427b90a8fe10a5
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2025-03-17 21:24:44 -0300

    feat: utiliza interface strategy

--- src/main/java/br/senac/menota/services/ProjetoService.java
+++ src/main/java/br/senac/menota/services/ProjetoService.java
@@ -2,17 +2,23 @@ package br.senac.menota.services;
 
 import br.senac.menota.model.Projeto;
 import br.senac.menota.repositories.ProjetoRepository;
-import br.senac.menota.repositories.StartupRepository;
+import br.senac.menota.strategy.NewProjetoValidationStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProjetoService {
 
     private final ProjetoRepository projetoRepository;
+    private final List<NewProjetoValidationStrategy> newProjetoValidationStrategies;
 
     public Projeto create(Projeto projeto){
+
+        newProjetoValidationStrategies.forEach(validation -> validation.validate(projeto));
+
         return projetoRepository.save(projeto);
     }
 }
