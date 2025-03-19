7de4aa62dc1d36b8df199eb296b62f5c133741f3
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2025-03-19 19:41:51 -0300

    feat: usando apenas uma classe strategy para validar datas prevista de inicio e entrega

--- src/main/java/br/senac/menota/strategy/projeto/ValidateDataPrevista.java
+++ src/main/java/br/senac/menota/strategy/projeto/ValidateDataPrevista.java
@@ -8,9 +8,22 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 @Component
-public class ValidateDataPrevistaEntrega implements NewProjetoValidationStrategy {
+public class ValidateDataPrevista implements NewProjetoValidationStrategy {
     @Override
     public void validate(Projeto projeto) {
+
+        if (projeto.getDataPrevistaInicio() == null){
+            throw new ValidationException("Data prevista de Início não pode estar em branco");
+        }
+
+        if (projeto.getDataPrevistaEntrega() == null){
+            throw new ValidationException("Data prevista de Entrega não pode estar em branco");
+        }
+
+        if (projeto.getDataPrevistaInicio().isBefore(LocalDate.now())){
+            throw new ValidationException("Data prevista de Início não pode ser menor que a data atual!");
+        }
+
         if (projeto.getDataPrevistaEntrega().isBefore(LocalDate.now())){
             throw new ValidationException("Data prevista de Entrega não pode ser menor que a data atual!");
         }
