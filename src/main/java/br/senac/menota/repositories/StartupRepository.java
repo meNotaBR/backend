7c2525ae6dde57363caafdf934413faf0c1500a4
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2025-03-19 20:02:29 -0300

    feat: add métodos findByCnpj e findByNomeFantasia

--- src/main/java/br/senac/menota/repositories/StartupRepository.java
+++ src/main/java/br/senac/menota/repositories/StartupRepository.java
@@ -3,5 +3,10 @@ package br.senac.menota.repositories;
 import br.senac.menota.model.Startup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StartupRepository extends JpaRepository<Startup, Long> {
+
+    Optional<Startup> findByCnpj(String cnpj);
+    Optional<Startup> findByNomeFantasia(String nomeFantasia);
 }
