29671f29131f966172576f8e4ff2c384e92e7264
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2025-03-19 20:03:52 -0300

    feat: usando interface strategy na criação de novas startups

--- src/main/java/br/senac/menota/services/StartupService.java
+++ src/main/java/br/senac/menota/services/StartupService.java
@@ -3,6 +3,7 @@ package br.senac.menota.services;
 import br.senac.menota.exceptions.NotFoundException;
 import br.senac.menota.model.Startup;
 import br.senac.menota.repositories.StartupRepository;
+import br.senac.menota.strategy.NewStartupValidationStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,12 @@ import java.util.List;
 public class StartupService {
 
     private final StartupRepository startupRepository;
+    private final List<NewStartupValidationStrategy> newStartupValidationStrategies;
 
     public Startup create(Startup request){
+
+        newStartupValidationStrategies.forEach(validation -> validation.validate(request));
+
         return startupRepository.save(request);
     }
 
