da9d5cbb53fd119fc77ba346702bedddfc397082
juanc <juancarlosbatistazeferino@gmail.com>
2025-03-22 18:43:51 -0300

    feat: add model empresário

---
+++ src/main/java/br/senac/menota/model/Empresario.java
@@ -0,0 +1,33 @@
+package br.senac.menota.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Entity(name = "tb_empresario")
+public class Empresario extends EntityID{
+
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String email;
+    private String numeroCelular;
+    private LocalDate dataCadastro;
+
+    @OneToOne(mappedBy = "empresario", fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private Startup startup;
+
+    @PrePersist
+    protected void onCreate(){
+        this.dataCadastro = LocalDate.now();
+    }
+}
