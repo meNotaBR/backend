2bdfc2e333bee058acb62a9f0f0169cbd3dce743
juanc <juancarlosbatistazeferino@gmail.com>
2025-03-22 18:47:55 -0300

    feat: add class empresario controller

---
+++ src/main/java/br/senac/menota/resources/EmpresarioController.java
@@ -0,0 +1,21 @@
+package br.senac.menota.resources;
+
+import br.senac.menota.model.Empresario;
+import br.senac.menota.services.EmpresarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/empresario")
+@RequiredArgsConstructor
+public class EmpresarioController {
+
+    private final EmpresarioService empresarioService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Empresario create(@RequestBody Empresario empresario){
+        return empresarioService.create(empresario);
+    }
+}
