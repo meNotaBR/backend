3e05101b39d994c335e444532aef393c971b9af9
JuannCarloss <juancarlosbatistazeferino@gmail.com>
2025-04-04 19:51:27 -0300

    feat: add validação para post ja curtidos pelo usuario

--- src/main/java/br/senac/menota/services/UpvoteService.java
+++ src/main/java/br/senac/menota/services/UpvoteService.java
@@ -1,5 +1,6 @@
 package br.senac.menota.services;
 
+import br.senac.menota.exceptions.ValidationException;
 import br.senac.menota.model.Upvote;
 import br.senac.menota.repositories.UpvoteRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,11 @@ public class UpvoteService {
     private final UpvoteRepository upvoteRepository;
 
     public Upvote create(Upvote upvote){
+
+        if (upvoteRepository.existsByInvestidorIdAndProjetoId(upvote.getInvestidor().getId(), upvote.getProjeto().getId())){
+            throw new ValidationException("ja laikado");
+        }
+
         return upvoteRepository.save(upvote);
     }
 }
