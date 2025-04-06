7a32f340d5ceeb7113fb1846785c04c81fdef27e
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-06 19:18:41 -0300

    feat: add metodo findByEmail

--- src/main/java/br/senac/menota/repositories/InvestidorRepository.java
+++ src/main/java/br/senac/menota/repositories/InvestidorRepository.java
@@ -4,4 +4,7 @@ import br.senac.menota.model.Investidor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvestidorRepository extends JpaRepository<Investidor, Long> {
+
+    Investidor findByEmail(String email);
+
 }
