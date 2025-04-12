a7822e99ee4198de36ab5d67416722fc553d6dd1
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-12 20:12:08 -0300

    feat: add user baseado no token e metodo de delete

--- src/main/java/br/senac/menota/services/UpvoteService.java
+++ src/main/java/br/senac/menota/services/UpvoteService.java
@@ -2,6 +2,7 @@ package br.senac.menota.services;
 
 import br.senac.menota.exceptions.ValidationException;
 import br.senac.menota.model.Upvote;
+import br.senac.menota.repositories.UpvoteCountRepository;
 import br.senac.menota.repositories.UpvoteRepository;
 import br.senac.menota.utils.AuthenticationUtil;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +13,36 @@ import org.springframework.stereotype.Service;
 public class UpvoteService {
 
     private final UpvoteRepository upvoteRepository;
+    private final UpvoteCountRepository upvoteCountRepository;
 
     public Upvote create(Upvote upvote){
 
-        if (upvoteRepository.existsByInvestidorIdAndProjetoId(AuthenticationUtil.retriveAuthenticatedUser().getId(), upvote.getProjeto().getId())){
+        if (upvoteRepository.existsByUserIdAndProjetoId(AuthenticationUtil.retriveAuthenticatedUser().getId(), upvote.getProjeto().getId())){
             throw new ValidationException("ja laikado");
         }
 
+        upvote.setUserId(AuthenticationUtil.retriveAuthenticatedUser().getId());
+
+        var count = upvoteCountRepository.findByProjetoId(upvote.getProjeto().getId());
+
+        count.setCount(count.getCount() + 1);
+
+        upvoteCountRepository.save(count);
+
         return upvoteRepository.save(upvote);
+    }
+
+    public void deleteUpvote(Upvote upvote){
+
+        var upvoteFind = upvoteRepository.findByUserIdAndProjetoId(AuthenticationUtil.retriveAuthenticatedUser().getId(), upvote.getProjeto().getId());
+
+        upvoteRepository.delete(upvoteFind);
+
+        var count = upvoteCountRepository.findByProjetoId(upvote.getProjeto().getId());
+
+        count.setCount(count.getCount() - 1);
+
+        upvoteCountRepository.save(count);
+
     }
 }
