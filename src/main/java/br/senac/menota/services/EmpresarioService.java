bae3aed755278399a8e51f593dbeb55d1feaa093
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-12 20:09:54 -0300

    feat: add criação de startup no cadastro

--- src/main/java/br/senac/menota/services/EmpresarioService.java
+++ src/main/java/br/senac/menota/services/EmpresarioService.java
@@ -1,12 +1,13 @@
 package br.senac.menota.services;
 
+import br.senac.menota.dtos.EmpresarioCreateRequestDTO;
 import br.senac.menota.dtos.EmpresarioCreateResponseDTO;
 import br.senac.menota.model.Empresario;
 import br.senac.menota.repositories.EmpresarioRepository;
+import br.senac.menota.repositories.StartupRepository;
 import br.senac.menota.strategy.NewEmpresarioValidationStrategy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,15 +18,24 @@ import java.util.List;
 public class EmpresarioService {
 
     private final EmpresarioRepository empresarioRepository;
+    private final StartupRepository startupRepository;
     private final List<NewEmpresarioValidationStrategy> newEmpresarioValidationStrategies;
 
-    public EmpresarioCreateResponseDTO create(Empresario empresario){
+    public EmpresarioCreateResponseDTO create(EmpresarioCreateRequestDTO request){
 
-        newEmpresarioValidationStrategies.forEach(validation -> validation.validate(empresario));
+        var empresarioFromRequest = EmpresarioCreateRequestDTO.fromEntity(request);
 
-        empresario.setSenha(new BCryptPasswordEncoder().encode(empresario.getSenha()));
+        newEmpresarioValidationStrategies.forEach(validation -> validation.validate(empresarioFromRequest));
 
-        return EmpresarioCreateResponseDTO.fromEntity(empresarioRepository.save(empresario));
+        var empresario = empresarioRepository.save(empresarioFromRequest);
+
+        var startup = EmpresarioCreateRequestDTO.fromEntityStartup(request);
+
+        startup.setEmpresario(empresario);
+
+        startupRepository.save(startup);
+
+        return EmpresarioCreateResponseDTO.fromEntity(empresario);
     }
 
     public List<Empresario> getAll(){
