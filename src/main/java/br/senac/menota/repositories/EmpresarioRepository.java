206a1e7cd4b3737b929fe8dfe42b72594e126e75
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-12 20:07:36 -0300

    feat: muda id de long para uuid

--- src/main/java/br/senac/menota/repositories/EmpresarioRepository.java
+++ src/main/java/br/senac/menota/repositories/EmpresarioRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface EmpresarioRepository extends JpaRepository<Empresario, Long> {
+public interface EmpresarioRepository extends JpaRepository<Empresario, String> {
 
     Optional<Empresario> findByCpf(String cpf);
     Empresario findByEmail(String email);
