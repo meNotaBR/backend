1208da3e6b8e3df8339f96f5bea519689f754a20
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-12 20:11:16 -0300

    feat: add metodo find by projeto id

--- src/main/java/br/senac/menota/repositories/UpvoteCountRepository.java
+++ src/main/java/br/senac/menota/repositories/UpvoteCountRepository.java
@@ -4,4 +4,6 @@ import br.senac.menota.model.UpvoteCount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UpvoteCountRepository extends JpaRepository<UpvoteCount, Long> {
+
+    UpvoteCount findByProjetoId(Long id);
 }
