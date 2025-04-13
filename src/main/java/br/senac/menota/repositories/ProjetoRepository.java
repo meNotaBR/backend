87ad95a12b57a31692ae1625838de268f1f03ffd
juanc <juancarlosbatistazeferino@gmail.com>
2025-04-13 01:59:42 -0300

    feat: add expecification executor

--- src/main/java/br/senac/menota/repositories/ProjetoRepository.java
+++ src/main/java/br/senac/menota/repositories/ProjetoRepository.java
@@ -2,6 +2,7 @@ package br.senac.menota.repositories;
 
 import br.senac.menota.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
+public interface ProjetoRepository extends JpaRepository<Projeto, Long>, JpaSpecificationExecutor<Projeto> {
 }
