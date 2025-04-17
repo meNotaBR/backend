ff555a19a68b3049741ea1208ef0bb6ee3f850ab
Carrerogabriel <132373775+Carrerogabriel@users.noreply.github.com>
2025-04-16 21:03:51 -0300

    Feat: Add methodo findAllByStartupId

--- src/main/java/br/senac/menota/repositories/ProjetoRepository.java
+++ src/main/java/br/senac/menota/repositories/ProjetoRepository.java
@@ -4,5 +4,9 @@ import br.senac.menota.model.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface ProjetoRepository extends JpaRepository<Projeto, Long>, JpaSpecificationExecutor<Projeto> {
+
+    List<Projeto> findAllByStartupId(Long startupId);
 }
