780e5735e6bc9eff131cf22ab1bc7593e2ae80eb
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2025-04-28 20:39:16 -0300

    feat: add método com query para agrupar upvotes por data

--- src/main/java/br/senac/menota/repositories/UpvoteRepository.java
+++ src/main/java/br/senac/menota/repositories/UpvoteRepository.java
@@ -1,10 +1,21 @@
 package br.senac.menota.repositories;
 
+import br.senac.menota.dtos.UpvoteGroupedByDate;
 import br.senac.menota.model.Upvote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UpvoteRepository extends JpaRepository<Upvote, Long> {
+import java.util.List;
+
+public interface UpvoteRepository extends JpaRepository<Upvote, Long>, JpaSpecificationExecutor<Upvote> {
 
     Upvote findByUserIdAndProjetoId(String userId, Long projetoId);
     boolean existsByUserIdAndProjetoId(String userId, Long projetoId);
+
+    @Query("SELECT new br.senac.menota.dtos.UpvoteGroupedByDate(u.dataCriacao, COUNT(u)) " +
+            "FROM tb_upvote u WHERE u.projeto.id = :projetoId " +
+            "GROUP BY u.dataCriacao ORDER BY u.dataCriacao DESC")
+    List<UpvoteGroupedByDate> findAllDescByProjetoId(@Param("projetoId") Long projetoId);
 }
