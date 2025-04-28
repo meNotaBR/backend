c475076e25e07b01e9b791302865bc82a23946f7
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2025-04-28 20:38:32 -0300

    feat: add endpoint para achar upvotes agrupados por data pelo id do projeto

--- src/main/java/br/senac/menota/resources/UpvoteController.java
+++ src/main/java/br/senac/menota/resources/UpvoteController.java
@@ -1,10 +1,13 @@
 package br.senac.menota.resources;
 
+import br.senac.menota.dtos.UpvoteGroupedByDate;
 import br.senac.menota.model.Upvote;
 import br.senac.menota.services.UpvoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/upvote")
@@ -23,5 +26,11 @@ public class UpvoteController {
     @ResponseStatus(HttpStatus.OK)
     public void delete(@RequestBody Upvote upvote){
         upvoteService.deleteUpvote(upvote);
+    }
+
+    @GetMapping("/projeto/{projetoId}/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UpvoteGroupedByDate> findAll(@PathVariable("projetoId") Long id){
+        return upvoteService.findAllByProjetoId(id);
     }
 }
