4ec3b6daa9dbf332ac6baf8155c8e78c65c144e8
juanc <juancarlosbatistazeferino@gmail.com>
2025-05-11 20:03:10 -0300

    feat: add metodo buscar todos entregaveis

--- src/main/java/br/senac/menota/repositories/EntregavelRepository.java
+++ src/main/java/br/senac/menota/repositories/EntregavelRepository.java
@@ -3,5 +3,9 @@ package br.senac.menota.repositories;
 import br.senac.menota.model.Entregavel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EntregavelRepository extends JpaRepository<Entregavel, Long> {
+
+    List<Entregavel> findAllByProjetoIdOrderByDataCriacaoAsc(Long id);
 }
