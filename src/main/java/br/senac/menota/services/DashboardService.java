066f32ca93e2ae1b8d3e7c32cd20dcad34c59754
juanc <juancarlosbatistazeferino@gmail.com>
2025-05-11 20:04:26 -0300

    feat: add metodo para buscar entregaveis do projeto

--- src/main/java/br/senac/menota/services/DashboardService.java
+++ src/main/java/br/senac/menota/services/DashboardService.java
@@ -2,6 +2,7 @@ package br.senac.menota.services;
 
 import br.senac.menota.model.DashboardView;
 import br.senac.menota.repositories.DashboardViewRepository;
+import br.senac.menota.repositories.EntregavelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Service;
 public class DashboardService {
 
     private final DashboardViewRepository dashboardViewRepository;
+    private final EntregavelRepository entregavelRepository;
     private final UpvoteService upvoteService;
 
     public DashboardView getViewByProjetoId(Long id){
         DashboardView view = dashboardViewRepository.dashboardViewByProjectId(id);
 
         view.setUpvotes(upvoteService.findAllByProjetoId(id));
+        view.setEntregaveis(entregavelRepository.findAllByProjetoIdOrderByDataCriacaoAsc(id));
 
         return view;
     }
